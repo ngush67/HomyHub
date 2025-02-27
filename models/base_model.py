42cebf30280ed4f89b55a716a47b84078482bdf0
Ngush67 <olsonngula@gmail.com>
2025-02-27 03:28:45 +0300

    Updated BaseModel to integrate with FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,42 @@
+#!/usr/bin/python3
+"""BaseModel class that defines common attributes/methods for other classes."""
 
 import uuid
 from datetime import datetime
+from models import storage  # Import storage instance
 
 class BaseModel:
-    """Base class for other models"""
+    """Defines all common attributes/methods for other classes."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new instance or recreate one from a dictionary"""
+        """Initialize a new instance or recreate from dictionary."""
         if kwargs:
-            # Recreate instance from a dictionary
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.fromisoformat(value))
-                elif key != "__class__":  # Ignore the __class__ key
-                    setattr(self, key, value)
+                if key != "__class__":  # Exclude __class__ attribute
+                    if key in ("created_at", "updated_at"):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
         else:
-            # Create a new instance
             self.id = str(uuid.uuid4())  # Generate unique ID
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def to_dict(self):
-        """Convert instance attributes to a dictionary"""
-        instance_dict = self.__dict__.copy()
-        instance_dict["__class__"] = self.__class__.__name__
-        instance_dict["created_at"] = self.created_at.isoformat()
-        instance_dict["updated_at"] = self.updated_at.isoformat()
-        return instance_dict
+            self.updated_at = self.created_at
+            storage.new(self)  # Register new instance in storage
 
     def __str__(self):
-        """Return a string representation of the object"""
+        """Return string representation of the instance."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update the updated_at attribute to current time"""
+        """Update `updated_at` timestamp and save instance to storage."""
         self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Return dictionary representation of the instance."""
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
 
